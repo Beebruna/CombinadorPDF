8b11d28754a24e33d54c971d9b9089865d1e43d7
enzocunha <70650870+enzocunha@users.noreply.github.com>
2020-09-28 15:58:54 -0300

    Update Application.py
    adicionando botoes adicionar e excluir e otimizando o codigo

--- Application.py
+++ Application.py
@@ -4,56 +4,39 @@ from tkinter import ttk
 
 class Application:
     def __init__(self, master):
-        "criando widgets na tela inicial"
+        "cria widgets na tela inicial"
         self.master = master
-        self.criar_menu()
         
-    def criar_menu(self):
-        "define todas as opçoes do menu"
-        self.menu = Menu(self.master)
-        self.master.config(menu=self.menu)
-
-        # criando itens do menu
-        self.menu_arquivos = Menu(self.menu)
-        self.menu.add_cascade(label='Arquivos', menu=self.menu_arquivos)
-        
-        # criando opçoes em cada item
-        self.menu_arquivos.add_command(label='Abrir arquivos', command=self.selecionar_arquivos)
-        self.menu_arquivos.add_separator()
-        self.menu_arquivos.add_command(label='Sair', command=self.master.quit)
-        
-    def selecionar_arquivos(self):
-        "abre janela de seleção de arquivos e separa os arquivos em formato pdf"
-        arquivos_selecionados = askopenfilenames()
-        self.lista_arquivos = []
-        for a in arquivos_selecionados:
-            if a.endswith('.pdf'):
-                self.lista_arquivos.append(a)
-        
-        # mostrando arquivos em uma tabela
-        self.criar_tabela()
-        
-    def criar_tabela(self):
-        '''
-        Cria uma tabela usando treeview
-        Mostra os arquivos selecionados pelo usuario de
-        acordo com a ordem da lista
-        '''
-        # definindo os widgets
+        # criando e configurando tabela usando treeview
         self.frame_tabela = Frame(self.master)
         self.tabela = ttk.Treeview(self.frame_tabela)
         
-        # configurando a tabela
         self.tabela.heading('#0', text='Arquivos selecionados')
         self.tabela.column('#0', stretch=True)
         
-        # inserindo os itens
-        for arquivo in self.lista_arquivos:
-            self.tabela.insert(parent='', index='end', text=arquivo)
-        
-        # mostrando widgets
         self.frame_tabela.pack(fill=BOTH, expand=1)
-        self.tabela.pack(fill=X, expand=1, pady=10)      
+        self.tabela.pack(fill=X, expand=1, pady=10)
+        
+        # criando e configurando botoes para manipular a tabela
+        self.botao_adicionar = Button(self.frame_tabela, text='Adicionar', command=self.adicionar_arquivos)
+        self.botao_excluir = Button(self.frame_tabela, text='Excluir', command=self.excluir_arquivos)
+        
+        self.botao_adicionar.pack()
+        self.botao_excluir.pack()
+        
+        
+    def adicionar_arquivos(self):
+        "abre janela de seleção de arquivos e adiciona os arquivos em formato pdf"
+        arquivos_selecionados = askopenfilenames()
+        for a in arquivos_selecionados:
+            if a.endswith('.pdf'):
+                self.tabela.insert(parent='', index='end', text=a)
+        
+    def excluir_arquivos(self):
+        "exclui os arquivos selecionados da tabela"
+        arquivos_selecionados = self.tabela.selection()
+        for a in arquivos_selecionados:
+            self.tabela.delete(a)    
 
 
 
