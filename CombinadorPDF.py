da3d2ce57e77e3d073368aafd923899a1d48cb2e
enzocunha <70650870+enzocunha@users.noreply.github.com>
2020-10-02 18:38:27 -0300

    Update CombinadorPDF.py
    ajustando atributos para maior compatibilidade

--- CombinadorPDF.py
+++ CombinadorPDF.py
@@ -2,20 +2,22 @@ from PIL import ImageTk, Image
 from fitz import *
 
 class CombinadorPDF:
-    
-    @staticmethod
-    def get_images(nome_arquivo):
-        "retorna o numero de paginas e uma lista de objetos PhotoImage para serem usados no tkinter"
-        pdf = fitz.open(nome_arquivo)
-        tamanho = len(pdf)
+    def __init__(self, nome_arquivo):
+        self.pdf = fitz.open(nome_arquivo)
+
+    def extrair_dados(self):
+        '''
+        calcula o numero de paginas do pdf
+        cria uma lista com objetos PhotoImage para serem usados na GUI
+        '''
+        # numero de paginas
+        self.tamanho_arquivo = len(self.pdf)
         
-        # redimensionando e adicionando imagem a lista
-        lista_imagens = []
-        for pagina in range(tamanho):
-            pix = pdf.getPagePixmap(pagina)
+        # criando lista com objetos PhotoImage
+        self.lista_imagens = []
+        for pagina in range(self.tamanho_arquivo):
+            pix = self.pdf.getPagePixmap(pagina)
             mode = 'RGBA' if pix.alpha else 'RGB'
             image = Image.frombytes(mode, [pix.width, pix.height], pix.samples)
             image = image.resize((round(680/pix.height*pix.width), round(680)))
-            lista_imagens.append(ImageTk.PhotoImage(image))
-        
-        return lista_imagens, tamanho
+            self.lista_imagens.append(ImageTk.PhotoImage(image))
