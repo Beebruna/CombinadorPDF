44ecbbb09efca886ac105ecf37f17bc86ca25535
enzocunha <70650870+enzocunha@users.noreply.github.com>
2020-10-05 16:08:40 -0300

    Update CombinadorPDF.py
    resolvendo problemas de compatibilidade

--- CombinadorPDF.py
+++ CombinadorPDF.py
@@ -2,22 +2,58 @@ from PIL import ImageTk, Image
 from fitz import *
 
 class CombinadorPDF:
-    def __init__(self, nome_arquivo):
-        self.pdf = fitz.open(nome_arquivo)
+    def __init__(self):
+        self.pdf = {}
+        self.nome_arquivos = []
 
-    def extrair_dados(self):
+    def adicionar_arquivos(self, nome_arquivos):
+        "insere arquivos nos atributos da classe"
+        for nome_arquivo in nome_arquivos:
+            self.nome_arquivos.append(nome_arquivo)
+            self.pdf[nome_arquivo] = fitz.open(nome_arquivo)
+
+    def excluir_arquivos(self, nome_arquivos):
+        "remove arquivos dos atributos da classe"
+        for nome_arquivo in nome_arquivos:
+            self.nome_arquivos.remove(nome_arquivo)
+            self.pdf.pop(nome_arquivo)
+
+    def mover_cima(self, nome_arquivo):
+        "troca de lugar o arquivo e seu antecessor na lista nome_arquivos"
+
+        index = self.nome_arquivos.index(nome_arquivo)
+        if index == 0:
+            return
+        
+        self.nome_arquivos.remove(nome_arquivo)
+        self.nome_arquivos.insert(index - 1, nome_arquivo)
+        
+    def mover_baixo(self, nome_arquivo):
+        "troca de lugar o arquivo e o seu sucessor na lista nome_arquivos"
+        
+        index = self.nome_arquivos.index(nome_arquivo)
+        if index == len(self.nome_arquivos) - 1:
+            return
+        
+        self.nome_arquivos.remove(nome_arquivo)
+        self.nome_arquivos.insert(index + 1, nome_arquivo)
+
+    def extrair_dados(self, nome_arquivo):
         '''
-        calcula o numero de paginas do pdf
-        cria uma lista com objetos PhotoImage para serem usados na GUI
+        calcula o numero de paginas do pdf selecionado
+        cria uma lista com objetos PhotoImage das paginas para serem usados na GUI
         '''
         # numero de paginas
-        self.tamanho_arquivo = len(self.pdf)
-        
+        pdf = self.pdf[nome_arquivo]
+        tamanho_arquivo = len(pdf)
+
         # criando lista com objetos PhotoImage
-        self.lista_imagens = []
-        for pagina in range(self.tamanho_arquivo):
-            pix = self.pdf.getPagePixmap(pagina)
+        lista_imagens = []
+        for pagina in range(tamanho_arquivo):
+            pix = pdf.getPagePixmap(pagina)
             mode = 'RGBA' if pix.alpha else 'RGB'
             image = Image.frombytes(mode, [pix.width, pix.height], pix.samples)
             image = image.resize((round(680/pix.height*pix.width), round(680)))
-            self.lista_imagens.append(ImageTk.PhotoImage(image))
+            lista_imagens.append(ImageTk.PhotoImage(image))
+
+        return lista_imagens, tamanho_arquivo
